50ef6c42791095ef3e05d4cc1a325f5ff522f5d6
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-04-30 13:35:44 +0600

    Добавлены тесты на консистенцию messages bundle

--- core/src/test/java/com/lapsa/country/test/CountryNamesMessagesBundleTest.java
+++ core/src/test/java/com/lapsa/country/test/CountryNamesMessagesBundleTest.java
@@ -10,11 +10,22 @@ import org.junit.Test;
 
 import com.lapsa.country.Country;
 
-public class TestCountryBundle {
+public class CountryNamesMessagesBundleTest {
 
     @Test
     public void testRussianBundle() {
 	ResourceBundle resources = ResourceBundle.getBundle(Country.BUNDLE_BASENAME, Locale.forLanguageTag("ru"));
+	assertThat(resources, not(nullValue()));
+
+	for (Country c : Country.values()) {
+	    String name = resources.getString(String.format("%s.%s", c.getClass().getName(), c.name()));
+	    assertThat(name, not(nullValue()));
+	}
+    }
+
+    @Test
+    public void testKazakhBundle() {
+	ResourceBundle resources = ResourceBundle.getBundle(Country.BUNDLE_BASENAME, Locale.forLanguageTag("kk"));
 	assertThat(resources, not(nullValue()));
 
 	for (Country c : Country.values()) {
---
+++ core/src/test/java/com/lapsa/country/test/MessagesBundleTest.java
@@ -0,0 +1,65 @@
+package com.lapsa.country.test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.Test;
+
+import com.lapsa.country.Country;
+
+public class MessagesBundleTest {
+
+    @Test
+    public void testNoExcessRecordsRussian() {
+	Locale locale = Locale.forLanguageTag("ru");
+	assertThat(locale.getLanguage(), allOf(not(nullValue()), is("ru")));
+	ResourceBundle resources = ResourceBundle.getBundle(Country.BUNDLE_BASENAME, locale);
+	assertThat(resources, not(nullValue()));
+	assertThat(resources.getString("com.lapsa.country.Country.AND"), allOf(not(nullValue()), is("Андорра")));
+	testBundle(resources);
+    }
+
+    @Test
+    public void testNoExcessRecordsKazakh() {
+	Locale locale = Locale.forLanguageTag("kk");
+	assertThat(locale.getLanguage(), allOf(not(nullValue()), is("kk")));
+	ResourceBundle resources = ResourceBundle.getBundle(Country.BUNDLE_BASENAME, locale);
+	assertThat(resources, not(nullValue()));
+	assertThat(resources.getString("com.lapsa.country.Country.TLS"), allOf(not(nullValue()), is("Шығыс Тимор")));
+	testBundle(resources);
+    }
+
+    @Test
+    public void testNoExcessRecordsEnglish() {
+	Locale locale = Locale.forLanguageTag("en");
+	assertThat(locale.getLanguage(), allOf(not(nullValue()), is("en")));
+	ResourceBundle resources = ResourceBundle.getBundle(Country.BUNDLE_BASENAME, locale);
+	assertThat(resources, not(nullValue()));
+	assertThat(resources.getString("com.lapsa.country.Country.TLS"), allOf(not(nullValue()), is("Timor-Leste")));
+	testBundle(resources);
+    }
+
+    private void testBundle(ResourceBundle resources) {
+	Enumeration<String> keys = resources.getKeys();
+	while (keys.hasMoreElements()) {
+	    String key = keys.nextElement();
+	    Country city = findByFullName(Country.values(), key);
+	    if (city != null)
+		continue;
+	    fail(String.format("Resource bunddle key '%1$s' is not present", key));
+	}
+    }
+
+    private <T extends Enum<?>> T findByFullName(T[] values, String key) {
+	for (T c : values) {
+	    String name = String.format("%s.%s", c.getClass().getName(), c.name());
+	    if (name.equals(key))
+		return c;
+	}
+	return null;
+    }
+}
