f636c1f36d319deede6ea057a351f141a0375a6c
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-05 12:15:53 +0600

    bundle description in faces-config.xml defined

--- core/src/main/java/com/lapsa/country/BundleBase.java
+++ core/src/main/java/com/lapsa/country/BundleBase.java
@@ -2,4 +2,5 @@ package com.lapsa.country;
 
 public interface BundleBase {
     public static final String BUNDLE_BASENAME = "InternationalLocalization";
+    public static final String BUNDLE_VAR = "il";
 }
