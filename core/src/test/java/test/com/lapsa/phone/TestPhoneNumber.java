cc15ba5ada1f7770ddca4f8114c5f32653d514cc
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-12 17:19:44 +0600

    Upadted tests due to Change phone number parser algorythm

--- core/src/test/java/test/com/lapsa/phone/TestPhoneNumber.java
+++ core/src/test/java/test/com/lapsa/phone/TestPhoneNumber.java
@@ -3,7 +3,10 @@ package test.com.lapsa.phone;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+import java.util.logging.Logger;
+
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.lapsa.phone.CountryCode;
@@ -14,7 +17,14 @@ import com.lapsa.phone.PhoneNumberFactoryProvider;
 
 public class TestPhoneNumber {
 
-    PhoneNumberFactory factory;
+    private static Logger logger;
+
+    private PhoneNumberFactory factory;
+
+    @BeforeClass
+    public static void init() {
+	logger = Logger.getAnonymousLogger();
+    }
 
     @Before
     public void beforeTest() {
@@ -22,15 +32,20 @@ public class TestPhoneNumber {
     }
 
     private static void printFormat(PhoneNumber a) {
-	String FORMAT = "Country code: %1$s Country number: %2$s Area code: %3$s Number: %4$s";
-	System.out.println(a.getFormatted());
-	System.out.println(a.getPlain());
-	System.out.println(String.format(FORMAT, a.getCountryCode().name(), a.getCountryCode().getPhoneCode(),
-		a.getAreaCode(), a.getNumber()));
-	System.out.println();
+	logger.info(String.format("Formatted: '%1$s' Plain: '%2$s'", //
+		a.getFormatted(), // $1
+		a.getPlain() // $2
+	));
+	logger.info(String.format(//
+		"Country code: %1$s Country number: %2$s Area code: %3$s Number: %4$s", //
+		a.getCountryCode().name(), // $1
+		a.getCountryCode().getPhoneCode(), // $2
+		a.getAreaCode(), // $3
+		a.getNumber() // $4
+	));
     }
 
-    private void test(String value, PhoneNumber expecting, String expectingFormat) throws PhoneFormatException {
+    private void testStrict(String value, PhoneNumber expecting, String expectingFormat) throws PhoneFormatException {
 	PhoneNumber a = factory.parse(value);
 	printFormat(a);
 	assertThat(a, allOf(not(nullValue()), is(equalTo(expecting))));
@@ -38,39 +53,48 @@ public class TestPhoneNumber {
     }
 
     @Test
-    public void testFormat1() {
+    public void testStrictFormat1() {
 	try {
-	    test(" 8 (7272)-530721 ", new PhoneNumber(CountryCode.KZ, "727", "2530721"), "+7 (727) 2530721");
+	    testStrict(" 8 (7272)-530721 ", new PhoneNumber(CountryCode.KZ, "727", "2530721"), "+7 (727) 2530721");
 	} catch (PhoneFormatException e) {
 	    fail(e.getMessage());
 	}
     }
 
     @Test
-    public void testFormatRU1() {
+    public void testStrictFormatRU1() {
 	try {
-	    test(" 8 (963)-777-79 79", new PhoneNumber(CountryCode.RU, "963", "7777979"), "+7 (963) 7777979");
+	    testStrict(" 8 (963)-777-79 79", new PhoneNumber(CountryCode.RU, "963", "7777979"), "+7 (963) 7777979");
 	} catch (PhoneFormatException e) {
 	    fail(e.getMessage());
 	}
     }
 
     @Test
-    public void testFormat2() {
+    public void testStrictFormat2() {
 	try {
-	    test(" 8 (727)2-530721 ", new PhoneNumber(CountryCode.KZ, "727", "2530721"), "+7 (727) 2530721");
+	    testStrict(" 8 (727)2-530721 ", new PhoneNumber(CountryCode.KZ, "727", "2530721"), "+7 (727) 2530721");
+	} catch (PhoneFormatException e) {
+	    fail(e.getMessage());
+	}
+    }
+
+    @Test(expected = PhoneFormatException.class)
+    public void testStrictFormat3() throws PhoneFormatException {
+	testStrict(" 8 (2272)-530721 ", new PhoneNumber(CountryCode.KR, "272", "530721"), "+82 (272) 530721");
+    }
+
+    @Test
+    public void testStrictFormat4() {
+	try {
+	    testStrict(" 8 (701)937-7979", new PhoneNumber(CountryCode.KZ, "701", "9377979"), "+7 (701) 9377979");
 	} catch (PhoneFormatException e) {
 	    fail(e.getMessage());
 	}
     }
 
     @Test
-    public void testFormat3() throws PhoneFormatException {
-	test(" 8 (2272)-530721 ", new PhoneNumber(CountryCode.KR, "272", "530721"), "+82 (272) 530721");
-    }
-
-    @Test
-    public void testParseFormat() {
+    public void testStrictParseFormat() {
 	String[] numbers = new String[] { " 8 (7272)-530721 ", "+1 268-464-1234", "+56(7)9377979", "494357019377979",
 		"87272530721", "+218 91-2345678", "962 7 9012 3456 " };
 
@@ -90,5 +114,4 @@ public class TestPhoneNumber {
 	    }
 	}
     }
-
 }
