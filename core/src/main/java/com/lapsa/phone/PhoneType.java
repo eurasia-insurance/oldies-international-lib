0d0e00b3576f5c60e7c520ee28c82deb0dda1195
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-16 12:26:58 +0600

    code for font awesome

--- core/src/main/java/com/lapsa/phone/PhoneType.java
+++ core/src/main/java/com/lapsa/phone/PhoneType.java
@@ -3,16 +3,26 @@ package com.lapsa.phone;
 import com.lapsa.country.InternationalLocalizationBundleBase;
 
 public enum PhoneType implements InternationalLocalizationBundleBase {
-    MAIN, // основной телефон
-    FAX, // телефон факса
-    MOBILE, // мобильный телефон
-    WORK, // рабочий телефон
-    HOME, // домашний телефон
+    MAIN("fa-phone"), // основной телефон
+    FAX("fa-fax"), // телефон факса
+    MOBILE("fa-mobile"), // мобильный телефон
+    WORK("fa-phone"), // рабочий телефон
+    HOME("fa-phone"), // домашний телефон
     //
     ;
+
+    private final String fontAwesomeCodeIcon;
+
+    PhoneType(String fontAwesomeCode) {
+	this.fontAwesomeCodeIcon = fontAwesomeCode;
+    }
 
     @Override
     public String canonicalName() {
 	return String.format("%1$s.%2$s", this.getClass().getName(), name());
+    }
+
+    public String getFontAwesomeIcon() {
+	return fontAwesomeCodeIcon;
     }
 }
