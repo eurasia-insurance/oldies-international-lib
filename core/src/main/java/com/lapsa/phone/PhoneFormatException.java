0a3962ab481f5bdd86922090df698e5a49f95ec2
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-05-07 14:56:22 +0600

    extending constructor

--- core/src/main/java/com/lapsa/phone/PhoneFormatException.java
+++ core/src/main/java/com/lapsa/phone/PhoneFormatException.java
@@ -8,4 +8,8 @@ public class PhoneFormatException extends Exception {
 	super(message);
     }
 
+    public PhoneFormatException(String message, Throwable cause) {
+	super(message, cause);
+    }
+
 }
