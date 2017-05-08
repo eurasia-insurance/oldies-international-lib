036db7ec188d00722e0b83e892cac001468067bb
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-05-08 14:34:47 +0600

    phone number converter localized message

--- eelib/src/main/java/com/lapsa/phone/faces/converter/PhoneNumberConverter.java
+++ eelib/src/main/java/com/lapsa/phone/faces/converter/PhoneNumberConverter.java
@@ -1,11 +1,17 @@
 package com.lapsa.phone.faces.converter;
 
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
+import com.lapsa.phone.PhoneFormatException;
 import com.lapsa.phone.PhoneNumber;
+import com.lapsa.phone.PhoneNumbers;
 
 @FacesConverter(forClass = PhoneNumber.class)
 public class PhoneNumberConverter implements Converter {
@@ -14,7 +20,14 @@ public class PhoneNumberConverter implements Converter {
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
 	if (value == null || value.isEmpty())
 	    return null;
-	return PhoneNumber.fromString(value);
+	try {
+	    return PhoneNumbers.parse(value);
+	} catch (PhoneFormatException e) {
+	    ResourceBundle bundle = ResourceBundle.getBundle("ConversionMessages", context.getViewRoot().getLocale());
+	    String summary = bundle.getString(this.getClass().getName());
+	    FacesMessage msg = new FacesMessage(summary);
+	    throw new ConverterException(msg);
+	}
     }
 
     @Override
