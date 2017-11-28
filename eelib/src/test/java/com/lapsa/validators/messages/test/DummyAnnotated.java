cd02272938efc44fbc8e4f3de239589ec13e9209
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-28 18:43:23 +0600

    tests fixed

--- eelib/src/test/java/com/lapsa/validators/messages/test/DummyAnnotated.java
+++ eelib/src/test/java/com/lapsa/validators/messages/test/DummyAnnotated.java
@@ -2,15 +2,11 @@ package com.lapsa.validators.messages.test;
 
 import com.lapsa.international.cars.validators.ValidVINCode;
 import com.lapsa.international.country.validators.ValidCountry;
-import com.lapsa.international.internet.validators.ValidEmail;
-import com.lapsa.international.internet.validators.ValidURL;
 import com.lapsa.international.phone.validators.ValidPhoneNumber;
 
 public interface DummyAnnotated {
     @ValidVINCode
     @ValidCountry
-    @ValidEmail
-    @ValidURL
     @ValidPhoneNumber
     void dummy();
 }
